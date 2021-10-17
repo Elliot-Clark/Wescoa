a962b3eb76d1e8345e4e6e7e41789096ec53d66c
Elliot-Clark <etcclark@gmail.com>
2021-10-17 14:57:36 -0700

    Footer Added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Secondary from './Secondary/secondary.js'
 import Tertiary from './Tertiary/tertiary.js'
 import Forth from './Forth/forth.js'
 import Fifth from './Fifth/fifth.js'
+import Footer from './Footer/footer.js'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Tertiary />
       <Forth />
       <Fifth />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer/footer.js
@@ -0,0 +1,11 @@
+import './footer.css'
+
+let Footer = () => {
+    return (
+        <div id="footer">
+            © 2021 WESCOA USA | Inc All Rights Reserved | Website Design by Elliot Clark
+        </div>
+    )
+}
+
+export default Footer
