2289be6878dabf4d1063eff5bfbc4b9a36a1b4ac
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2021-11-01 23:08:08 -0700

    Images added

--- src/Secondary/ImageGallery/images.js
+++ src/Secondary/ImageGallery/images.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import './images.css'
+import image1 from './Images/image1.png'
+import image2 from './Images/image2.jpg'
+import image3 from './Images/image3.jpg'
+
 
 class ImageGallery extends Component {
     state = {
@@ -9,8 +13,15 @@ class ImageGallery extends Component {
     render() {
         return (
             <div id="imageGallery">
-                <div id="modal"></div>
-                feawfewf
+                <div id="modal">
+                    <button id="closeButton">X</button>
+                    <div id="images">
+                        <img src={image1} alt="DOHC Redhead 3"></img>
+                        <img src={image2} alt="Wescoa Engine"></img>
+                        <img src={image3} alt="Wescoa Engine"></img>
+
+                    </div>
+                </div>
             </div>
         )
     }
