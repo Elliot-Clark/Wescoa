1d4a737999aaf1a78909350d4279158bc249727d
Elliot-Clark <31608028+Elliot-Clark@users.noreply.github.com>
2021-11-03 20:56:12 -0700

    Images Added

--- src/Secondary/ImageGallery/images.js
+++ src/Secondary/ImageGallery/images.js
@@ -1,4 +1,3 @@
-import React, { Component } from "react";
 import './images.css'
 import image1 from './Images/image1.png'
 import image2 from './Images/image2.jpg'
@@ -22,45 +21,49 @@ import image19 from './Images/image19.jpg'
 import image20 from './Images/image20.jpg'
 import image21 from './Images/image21.jpg'
 import image22 from './Images/image22.jpg'
-//import image23 from './Images/image23.jpg'
+import image23 from './Images/image23.jpg'
+import image24 from './Images/image24.jpg'
+import image25 from './Images/image25.jpg'
+import image26 from './Images/image26.jpg'
+import image27 from './Images/image27.jpg'
+import image28 from './Images/image28.png'
 
-
-class ImageGallery extends Component {
-    state = {
-        
-    }
-
-    render() {
-        return (
-            <div id="imageGallery">
-                <button id="closeButton">X</button>
-                <div id="images">
-                    <a href={image1} rel="noreferrer" target="_blank"><img src={image1} alt="DOHC Redhead 3"></img></a>
-                    <a href={image2} rel="noreferrer" target="_blank"><img src={image2} alt="Wescoa Engine"></img></a>
-                    <a href={image3} rel="noreferrer" target="_blank"><img src={image3} alt="Wescoa Engine"></img></a>
-                    <a href={image4} rel="noreferrer" target="_blank"><img src={image4} alt="Wescoa Engine"></img></a>
-                    <a href={image5} rel="noreferrer" target="_blank"><img src={image5} alt="Wescoa Engine"></img></a>
-                    <a href={image6} rel="noreferrer" target="_blank"><img src={image6} alt="Wescoa Engine"></img></a>
-                    <a href={image7} rel="noreferrer" target="_blank"><img src={image7} alt="Wescoa Engine"></img></a>
-                    <a href={image8} rel="noreferrer" target="_blank"><img src={image8} alt="Wescoa Engine"></img></a>
-                    <a href={image9} rel="noreferrer" target="_blank"><img src={image9} alt="Wescoa Engine"></img></a>
-                    <a href={image10} rel="noreferrer" target="_blank"><img src={image10} alt="Wescoa Engine"></img></a>
-                    <a href={image11} rel="noreferrer" target="_blank"><img src={image11} alt="Wescoa Engine"></img></a>
-                    <a href={image12} rel="noreferrer" target="_blank"><img src={image12} alt="Wescoa Engine"></img></a>
-                    <a href={image13} rel="noreferrer" target="_blank"><img src={image13} alt="Wescoa Engine"></img></a>
-                    <a href={image14} rel="noreferrer" target="_blank"><img src={image14} alt="Wescoa Engine"></img></a>
-                    <a href={image15} rel="noreferrer" target="_blank"><img src={image15} alt="Wescoa Engine"></img></a>
-                    <a href={image16} rel="noreferrer" target="_blank"><img src={image16} alt="Wescoa Engine"></img></a>
-                    <a href={image17} rel="noreferrer" target="_blank"><img src={image17} alt="Wescoa Engine"></img></a>
-                    <a href={image18} rel="noreferrer" target="_blank"><img src={image18} alt="Wescoa Engine"></img></a>
-                    <a href={image19} rel="noreferrer" target="_blank"><img src={image19} alt="Wescoa Engine"></img></a>
-                    <a href={image20} rel="noreferrer" target="_blank"><img src={image20} alt="Wescoa Engine"></img></a>
-                    <a href={image21} rel="noreferrer" target="_blank"><img src={image21} alt="Wescoa Engine"></img></a>
-                    <a href={image22} rel="noreferrer" target="_blank"><img src={image22} alt="Wescoa Engine"></img></a>
-                </div>
+let ImageGallery = (props) => {
+    return (
+        <div id="imageGallery">
+            <button id="closeButton" onClick={props.modalToggle}>X</button>
+            <div id="images">
+                <a href="https://www.youtube.com/watch?v=szNDQMM2pYw" rel="noreferrer" target="_blank"><img src={image28} alt="EMC V 90 first drive"></img></a>
+                <a href={image23} rel="noreferrer" target="_blank"><img src={image23} alt="Wescoa at 2019 Silicon Valley Auto Show"></img></a>
+                <a href={image1} rel="noreferrer" target="_blank"><img src={image1} alt="DOHC Redhead 3"></img></a>
+                <a href={image2} rel="noreferrer" target="_blank"><img src={image2} alt="Wescoa Engine"></img></a>
+                <a href={image3} rel="noreferrer" target="_blank"><img src={image3} alt="Wescoa Engine"></img></a>
+                <a href={image4} rel="noreferrer" target="_blank"><img src={image4} alt="Wescoa Engine"></img></a>
+                <a href={image5} rel="noreferrer" target="_blank"><img src={image5} alt="Wescoa Engine"></img></a>
+                <a href={image6} rel="noreferrer" target="_blank"><img src={image6} alt="Wescoa Engine"></img></a>
+                <a href={image7} rel="noreferrer" target="_blank"><img src={image7} alt="Wescoa Engine"></img></a>
+                <a href={image8} rel="noreferrer" target="_blank"><img src={image8} alt="Wescoa Engine"></img></a>
+                <a href={image9} rel="noreferrer" target="_blank"><img src={image9} alt="Wescoa Engine"></img></a>
+                <a href={image10} rel="noreferrer" target="_blank"><img src={image10} alt="Wescoa Engine"></img></a>
+                <a href={image11} rel="noreferrer" target="_blank"><img src={image11} alt="Wescoa Engine"></img></a>
+                <a href={image12} rel="noreferrer" target="_blank"><img src={image12} alt="Wescoa Engine"></img></a>
+                <a href={image13} rel="noreferrer" target="_blank"><img src={image13} alt="Wescoa Engine"></img></a>
+                <a href={image14} rel="noreferrer" target="_blank"><img src={image14} alt="Wescoa Engine"></img></a>
+                <a href={image15} rel="noreferrer" target="_blank"><img src={image15} alt="Wescoa Engine"></img></a>
+                <a href={image16} rel="noreferrer" target="_blank"><img src={image16} alt="Wescoa Engine"></img></a>
+                <a href={image17} rel="noreferrer" target="_blank"><img src={image17} alt="Wescoa Engine"></img></a>
+                <a href={image18} rel="noreferrer" target="_blank"><img src={image18} alt="Wescoa Engine"></img></a>
+                <a href={image19} rel="noreferrer" target="_blank"><img src={image19} alt="Wescoa Engine"></img></a>
+                <a href={image20} rel="noreferrer" target="_blank"><img src={image20} alt="Wescoa Engine"></img></a>
+                <a href={image21} rel="noreferrer" target="_blank"><img src={image21} alt="Wescoa Engine"></img></a>
+                <a href={image22} rel="noreferrer" target="_blank"><img src={image22} alt="Wescoa Engine"></img></a>
+                <a href={image24} rel="noreferrer" target="_blank"><img src={image24} alt="1956 Porsche Speedster with Wescoa Engine"></img></a>
+                <a href={image25} rel="noreferrer" target="_blank"><img src={image25} alt="1956 Porsche Speedster with Wescoa Engine"></img></a>
+                <a href={image26} rel="noreferrer" target="_blank"><img src={image26} alt="1956 Porsche Speedster with Wescoa Engine"></img></a>
+                <a href={image27} rel="noreferrer" target="_blank"><img src={image27} alt="1956 Porsche Speedster with Wescoa Engine"></img></a>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default ImageGallery
--- src/Secondary/secondary.js
+++ src/Secondary/secondary.js
@@ -1,49 +1,71 @@
+import { Component } from "react";
 import './secondary.css'
 import secondaryImage1 from './V2-90 Motorcycle.jpg'
 import ImageGallery from './ImageGallery/images.js'
 
-let Secondary = () => {
-    return (
-        <div id="secondary">
-            <div id="secondaryWrapper">
-                <h2>Wescoa Engine Technology Inc.</h2>
+class Secondary extends Component {
+    state = {
+        modal: false
+    }
 
-                <div id="secondaryTextWrapper">
-                    <div id="topLeftSecondary">
-                        <p>
-                            WET (“Wescoa” or the “Company”) was formed in 2004 under the laws of California in order to design,
-                            produce and market state of the art lightweight high performance 4-stroke engines for 
-                            commercial / recreational vehicles and other consumer applications that outperform “standards” 
-                            for emissions, efficiency and fuel economy.
+    modalToggle = () => {
+        if (this.state.modal) {
+            this.setState({ modal: false});
+        } else {
+            this.setState({modal: true});
+        }
+    }
+
+    render() {
+        return (
+            <div id="secondary">
+                <div id="secondaryWrapper">
+                    <h2>Wescoa Engine Technology Inc.</h2>
+    
+                    <div id="secondaryTextWrapper">
+                        <div id="topLeftSecondary">
+                            <p>
+                                WET (“Wescoa” or the “Company”) was formed in 2004 under the laws of California in order to design,
+                                produce and market state of the art lightweight high performance 4-stroke engines for 
+                                commercial / recreational vehicles and other consumer applications that outperform “standards” 
+                                for emissions, efficiency and fuel economy.
+                            </p>
+                        </div>
+                        
+                        <br></br>
+                        <p id="topRightSecondary">
+                            The company engaged world class talent to develop this family of V2-90 internal combustion engines,
+                            which, were designed as the heart of the new hybrid power trains that will be utilized in further vehicles
+                            as well as the primary power source for smaller non-hybrid vehicles.
                         </p>
                     </div>
-                    
-                    <br></br>
-                    <p id="topRightSecondary">
-                        The company engaged world class talent to develop this family of V2-90 internal combustion engines,
-                        which, were designed as the heart of the new hybrid power trains that will be utilized in further vehicles
-                        as well as the primary power source for smaller non-hybrid vehicles.
-                    </p>
-                </div>
-
-                <h2 id="normalTitle">The Wescoa Mission:</h2>
-
-                <div id="secondaryImageWrapper">
-                    <div id="bottomLeftSecondary">
-                        <img src={secondaryImage1} alt="V2-90 Wescoa Engine Motorcycle"></img>
-                    </div>
-                    <h2 id="responsiveTitle">The Wescoa Mission:</h2>
-                    <div id="bottomRightSecondary">
-                        <ul>To promote and sell advanced engine technology</ul>
-                        <ul>Develop patent and leading edge technology for such engines</ul>
-                        <ul>License proprietary technology to major producers for global markets</ul>
-                        <button id="imageGalleryButton">Wescoa Image Gallery →</button>
-                        < ImageGallery />
+    
+                    <h2 id="normalTitle">The Wescoa Mission:</h2>
+    
+                    <div id="secondaryImageWrapper">
+                        <div id="bottomLeftSecondary">
+                            <img src={secondaryImage1} alt="V2-90 Wescoa Engine Motorcycle"></img>
+                        </div>
+                        <h2 id="responsiveTitle">The Wescoa Mission:</h2>
+                        <div id="bottomRightSecondary">
+                            <ul>To promote and sell advanced engine technology</ul>
+                            <ul>Develop patent and leading edge technology for such engines</ul>
+                            <ul>License proprietary technology to major producers for global markets</ul>
+                            <button id="imageGalleryButton" onClick={this.modalToggle}>Wescoa Image Gallery →</button>
+                            {this.state.modal ? 
+                                <ImageGallery
+                                modalToggle = {this.modalToggle}
+                                />
+                            : 
+                                ''
+                            }
+                            
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Secondary
